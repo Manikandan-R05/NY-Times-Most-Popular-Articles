2b243cca3e8dd9e9a2b42b92efe9a0d44ec8b35d
Manikandan <manikandan.ramesh@xische.com>
2024-03-29 19:00:22 +0530

    Revert "inital commit"
    This reverts commit 53040445da2871d570c939bfe1619e37713378b2.

--- cypress/integration/example.spec.js
+++
@@ -1,8 +0,0 @@
-// cypress/integration/example.spec.js
-import "@cypress/code-coverage/use-babelrc";
-
-describe("My test suite", () => {
-  it("should test something", () => {
-    // Your test code
-  });
-});
